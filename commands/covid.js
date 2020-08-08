0d5a1c872dacff74ed674ba398a4c2f1907c0f68
Ondřej Niesner <ondra.niesner@seznam.cz>
2020-08-08 23:23:46 +0200

    Added covid command

---
+++ commands/covid.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: 'covid',
+	description: 'Koronavirus',
+	async execute(msg, args) {
+
+        fetch('https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true')
+        .then(res => res.json())
+        .then(json => {
+
+            const embed = new MessageEmbed()
+            .setTitle('**:flag_cz: KORONAVIRUS**')
+            .setColor('#5cb85c')
+            .addFields(
+                { name: ':egg: Potvrzeno', value: json.infected, inline: true },
+                { name: ':syringe: Úmrtí', value: json.deceased, inline: true },
+                { name: ':pill: Vyléčení', value: json.recovered, inline: true }
+            )
+    
+            msg.channel.send(embed);
+
+        }).catch(err => msg.channel.send(':x: Nastala chyba pri získávání informací'));
+        
+	}
+};
