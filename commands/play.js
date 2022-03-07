467cbec81fb4a0eb0bd9034011d676d95b51a248
Ondřej Niesner <ondra.niesner@seznam.cz>
2022-03-07 19:52:25 +0100

    Use play-dl

--- commands/play.js
+++ commands/play.js
@@ -1,8 +1,7 @@
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { joinVoiceChannel, createAudioResource, createAudioPlayer, entersState, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
-const ytdl = require('ytdl-core')
-const ytsr = require('ytsr');
+const playdl = require('play-dl')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,8 +14,8 @@ module.exports = {
         const channel = inter.member.voice?.channel;
         if (!channel) return inter.followUp(':x: Musíš být v místnosti!');
     
-        let song = await ytsr(inter.options.getString('song'), { limit: 1 });
-        song = song.items[0];
+        const song = await playdl.search(inter.options.getString('song'), { limit: 1 });
+        const stream = await playdl.stream(song[0].url);
 
         if (!song) return inter.followUp(':x: Nenalezeno');
 
@@ -30,13 +29,13 @@ module.exports = {
             await entersState(connection, VoiceConnectionStatus.Ready);
 
             const player = createAudioPlayer();
-            const resource = createAudioResource(ytdl(song.url), { filter: 'audioonly' });
+            const resource = createAudioResource(stream.stream, { inputType: stream.type  });
             player.play(resource);
             connection.subscribe(player);
 
             await entersState(player, AudioPlayerStatus.Playing);
             
-            inter.followUp(`:white_check_mark: Přehrávám **${song.title}**`);
+            inter.followUp(`:notes: Přehrávám **${song[0].title}**`);
         } catch(error) {
             inter.followUp(`:x: Nepodařilo se přehrát skladbu.`)
             console.error(error);
