f383712ae9f4889ba24d58faaf16d22c65e798f1
Ondřej Niesner <ondra.niesner@seznam.cz>
2022-10-17 20:05:10 +0200

    Changed web domain

--- commands/web.js
+++ commands/web.js
@@ -1,36 +1,34 @@
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
+const { webToken } = require('../config.json');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('web')
-    .setDescription('Vytvoří web')
-    .addStringOption(option => option.setName('username').setDescription('Název uživatele').setRequired(true)),
-    async execute(inter) {
+	data: new SlashCommandBuilder()
+		.setName('web')
+		.setDescription('Vytvoří web')
+		.addStringOption((option) => option.setName('username').setDescription('Název uživatele').setRequired(true)),
+	async execute(inter) {
+		const username = inter.options.getString('username');
+		const usernameEncoded = encodeURI(username);
 
-        const username = inter.options.getString('username');
-        const usernameEncoded = encodeURI(username);
+		try {
+			const res = await fetch(`http://api.onni.cz/create?username=${username}&token=${webToken}`);
+			const data = await res.json();
 
-        try {
-            const res = await fetch(`http://127.0.0.1:3002/create?username=${username}`);
-            const data = await res.json();
-
-            const reply = `
-FTP Hostitel: zerocz.eu
+			const reply = `
+FTP Hostitel: web.onni.cz
 FTP Uživatel: \`${data[0]['ftp_username']}\`
 FTP Heslo: \`${data[0]['ftp_password']}\`
-MySQL Administrace: https://db.zerocz.eu
+MySQL Administrace: https://web.onni.cz
 MySQL Uživatel: \`${data[1]['db_username']}\`
 MySQL Heslo: \`${data[1]['db_password']}\`
-Web: http://${data[0]['ftp_username']}.bagetak.fun`;
+Web: http://${data[0]['ftp_username']}.web.onni.cz`;
 
-            inter.reply({ content: reply, ephemeral: true });
-            
-        } catch (err) {
-            console.log(err);
-            inter.reply({ content: ':x: Nastala chyba pri získávání informací', ephemeral: true });
-        }
-
-    }
+			inter.reply({ content: reply, ephemeral: true });
+		} catch (err) {
+			console.log(err);
+			inter.reply({ content: ':x: Nastala chyba pri získávání informací', ephemeral: true });
+		}
+	},
 };
